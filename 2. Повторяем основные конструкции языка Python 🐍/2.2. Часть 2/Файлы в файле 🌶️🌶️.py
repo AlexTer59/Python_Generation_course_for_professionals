9ac4209c7df398258a83955f9b433ec19b0a82e5
Alex <alex.ter.61660z2@gmail.com>
2024-03-05 03:01:17 +0500

    solved Файлы в файле 🌶️🌶️.py

--- 2. Повторяем основные конструкции языка Python 🐍/2.2. Часть 2/Файлы в файле 🌶️🌶️.py
+++ 2. Повторяем основные конструкции языка Python 🐍/2.2. Часть 2/Файлы в файле 🌶️🌶️.py
@@ -1,46 +1,49 @@
+UNITS = {
+        'B': 1,
+        'KB': 1024,
+        'MB': 1024 ** 2,
+        'GB': 1024 ** 3
+    }
+
+def to_bytes(size, size_type):
+    return UNITS[size_type.replace('\n', '')] * int(size)
+
+
+def to_true_size_type(size):
+    size_step = 1024
+    for unit in UNITS.keys():
+        if size < size_step:
+            return f'{round(size)} {unit}'
+        if unit == 'GB':
+            return f'{round(size)} GB'
+        size /= size_step
+
+
 with open('files.txt', 'r', encoding='utf-8') as file:
-    dct = {}
-    names = []
-    sizes = []
-    names_of_size = []
+    files_dct = {}
     for i in file.readlines():
-        name, size, name_of_size = i.split()
-        dct[name] = [size, name_of_size]
-sort = (sorted(dct.items(), key=lambda x: x[0].rsplit('.', 1)[1]))
-dct = dict(sort)
-temp = list(dct)[0]
-print(dct)
-list_type_of_size = ['B', 'KB', 'MB', 'GB']
-inx_type = 0
-for key, value in dct.items():
-    if list_type_of_size.index(value[1]) > inx_type:
-        inx_type = list_type_of_size.index(value[1])
+        name_and_type, size, size_type = i.split(' ')
+        files_dct[name_and_type] = [size, size_type]
+files_dct = dict(sorted(files_dct.items(), key=lambda x: x[0].rsplit('.', 1)[0]))
+files_dct = dict(sorted(files_dct.items(), key=lambda x: x[0].rsplit('.', 1)[1]))
+name_and_type = list(files_dct.keys())
+summary = 0
+for item in name_and_type:
+    summary += to_bytes(*files_dct[item])
+    try:
+        if item.split('.')[1] != name_and_type[name_and_type.index(item) + 1].split('.')[1]:
+            print(item)
+            print(10 * '-')
+            print(f'Summary: {to_true_size_type(summary)}')
+            print()
+            summary = 0
+        else:
+            print(item)
+    except(IndexError):
+        print(item)
+        print(10 * '-')
+        print(f'Summary: {to_true_size_type(summary)}')
 
 
 
-    #if value[1] == 'GB':
-        #value[0] = value[0] * 1073741824
-    #elif value[1] == 'MB':
-       # value[0] = value[0] * 1048576
-   # elif value[1] == 'MB':
-       # value[0] = value[0] * 1048576
-    #elif value[1] == 'KB':
-        #value[0] = value[0] * 1024
 
-
-
-    if key[key.index('.')::] not in temp:
-
-        print('----------')
-        print('Summary:', list_type_of_size[inx_type])
-        print()
-        print(key)
-        inx_type = 0
-    else:
-        print(key)
-    temp = key[key.index('.')::]
-
-print('----------')
-print('Summary:', list_type_of_size[inx_type])
-        #mem += int(value[0])
-#for i in range(len(names)):
