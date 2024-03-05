5acc6ff569737cba2ed42709dfb47bc383c71693
Alex <alex.ter.61660z2@gmail.com>
2024-03-06 03:50:40 +0500

    solved: Функция is_available_date() 🌶️

--- 3. Работа с датой и временем/3.3. Тип данных datetime/Функция is_available_date() 🌶️.py
+++ 3. Работа с датой и временем/3.3. Тип данных datetime/Функция is_available_date() 🌶️.py
@@ -1,26 +1,20 @@
 from datetime import datetime
-def is_available_date():
-    for i in dates:
-        pass
+
+def date_to_arr(date):
+    return set(i for i in range(datetime.strptime(date.split('-')[0], '%d.%m.%Y').toordinal(), datetime.strptime(
+        date.split('-')[-1], '%d.%m.%Y').toordinal() + 1))
 
 
+def is_available_date(booked_dates, date_for_booking):
+    booked = set()
+    for i in booked_dates:
+        booked.update(date_to_arr(i))
+    date_for_booking = date_to_arr(date_for_booking)
+    if date_for_booking & booked:
+        return False
+    return True
 
 
 dates = ['04.11.2021', '05.11.2021-09.11.2021']
-some_date = '01.11.2021'
+some_date = '01.11.2021-04.11.2021'
 print(is_available_date(dates, some_date))
-
-
-
-lst = [int(input()), int(input()), int(input())]
-# lst = [1, 2, 3]
-min_num = min(lst)
-#min_num = 1
-if min_num >= 0:
-    print(sum(lst))
-else:
-
-from functools import reduce
-    print(reduce(lambda x: if x > 0, lst))
-
-
