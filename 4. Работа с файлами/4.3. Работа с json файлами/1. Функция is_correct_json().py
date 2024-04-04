3260d2380e744fc0d60761e94866d3f3aea3bb0d
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-04-04 15:43:19 +0500

    solved: Функция is_correct_json().py

---
+++ 4. Работа с файлами/4.3. Работа с json файлами/1. Функция is_correct_json().py
@@ -0,0 +1,17 @@
+'''
+Реализуйте функцию is_correct_json(), которая принимает один аргумент:
+
+string — произвольная строка
+Функция должна возвращать True, если строка string удовлетворяет формату JSON, или False в противном случае.
+'''
+
+
+import json
+
+
+def is_correct_json(string):
+    try:
+        json.loads(string)
+    except json.decoder.JSONDecodeError:
+        return False
+    return True
